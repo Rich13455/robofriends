a5a62f21977410e7db54a259277135a7b271fbeb
Richard Arjoon <rich.arjoon@gmail.com>
2019-09-24 13:14:53 -0600

    Updates

---
+++ precache-manifest.abaf33ed31bb971815b8163b86064c2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "534f2db1604d89cf4c4a0922298ae648",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "a040063f4488ebf0856f",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "c55d767b29c0d31a4ba2",
+    "url": "/robofriends/static/css/main.b79dba18.chunk.css"
+  },
+  {
+    "revision": "a040063f4488ebf0856f",
+    "url": "/robofriends/static/js/2.791d43d6.chunk.js"
+  },
+  {
+    "revision": "c55d767b29c0d31a4ba2",
+    "url": "/robofriends/static/js/main.4b7808d7.chunk.js"
+  },
+  {
+    "revision": "8e199c7f211d8f157d3a",
+    "url": "/robofriends/static/js/runtime~main.a9498424.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
