519c1ecf3078663bdc28ff13ef24b9dcf7a47add
Richard Arjoon <rich.arjoon@gmail.com>
2020-01-03 15:30:42 -0600

    Setting up Circle Ci

--- src/components/CounterButton.js
+++ src/components/CounterButton.js
@@ -20,9 +20,9 @@ class CounterButton extends Component {
         })
     }
     render() {
-        console.log('CounterButton');
+        // console.log('CounterButton');
         return (
-            <button color={this.props.color} onClick={this.updateCount}>Count: {this.state.count}</button>
+            <button id='counter' color={this.props.color} onClick={this.updateCount}>Count: {this.state.count}</button>
         )
     }
 }
